53fd9178ea6ab8b86d8ed728b603970fcd355742
VinyProgramador <vinicius.nunes@sptech.school>
2022-08-19 13:33:43 -0300

    Continuando site institucional, adcionando api ViaCep

---
+++ public/js/cepSrc.js
@@ -0,0 +1,16 @@
+function buscarCep() {
+    var cep = inputCep.value;
+    var logradouro = inputLogradouro;
+    var uf = inputUf;
+    // console.log(cep)
+    var url = "https://viacep.com.br/ws/"+cep+"/json/"  
+    $.ajax({
+        url:url,
+        type:"GET",
+        success:function(response){
+            console.log(response)
+            logradouro.value = response.logradouro;
+            uf.value = response.uf;
+        }
+    })
+}
