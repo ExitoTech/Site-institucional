964248296abbd6da61b5ec6455d5b0172c860f64
VinyProgramador <vinicius.nunes@sptech.school>
2022-08-26 18:35:27 -0300

    Adicionando api switAlerts.

--- public/js/SignUp.js
+++ public/js/SignUp.js
@@ -1,14 +1,25 @@
-function nextSignUp() {
-    window.location.assign("cadastro-pt2.html");
-}
 
-function validarNome() {
+var errosValidacao = 0;
+
+
+function nextSignUp() {
+
     var nome = nomeDaEmpresa.value;
 
     if (nome.length < 3) {
         console.log('xpto');
+        errosValidacao++;
     }
-    else {
-        console.log('deu bom!')
+
+    else if (errosValidacao == 0) {
+        setTimeout(() => {
+            window.location.assign("cadastro-pt2.html");
+        }, "2500")
+
+        Swal.fire({
+            icon: 'success',
+            title: '<h3>Dados enviados com sucesso</h3> <br> Indo para a proxima etapa.',
+            showConfirmButton: false
+    })
     }
 }
