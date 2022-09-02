a4bb7704e179f972883408075b717737e2017372
Pedro <pedro.pires@sptech.school>
2022-09-02 14:45:15 -0300

    ajustes nas validações das inputs de cadastro

--- public/js/SignUp.js
+++ public/js/SignUp.js
@@ -2,20 +2,21 @@ var errosValidação = 0;
 
 function validarNomeEmpresa(){
     var nomeEmpresa = inputNomeDaEmpresa.value;
-    if(nomeEmpresa.length>=3){
-        inputNomeDaEmpresa.style.border = "2px solid green";
+    if(nomeEmpresa.length >= 3){
+        inputNomeDaEmpresa.style.color = "#80b918";
         inputNomeDaEmpresa.style.borderLeft = "none";
         inputNomeDaEmpresa.style.borderTop = "none";
         inputNomeDaEmpresa.style.borderRight = "none";
     }
     else if(nomeEmpresa == ""){
-        inputNomeDaEmpresa.style.border = "2px solid #b8860b";
+        inputNomeDaEmpresa.style.border = "2px solid #ffc300";
         inputNomeDaEmpresa.style.borderLeft = "none";
         inputNomeDaEmpresa.style.borderTop = "none";
         inputNomeDaEmpresa.style.borderRight = "none";  
     }
     else{
-        inputNomeDaEmpresa.style.border = "2px solid red";
+        inputNomeDaEmpresa.style.color = "#d90429";
+        inputNomeDaEmpresa.style.border = "2px solid #000633";
         inputNomeDaEmpresa.style.borderLeft = "none";
         inputNomeDaEmpresa.style.borderTop = "none";
         inputNomeDaEmpresa.style.borderRight = "none";  
@@ -26,20 +27,21 @@ function verificarEmail() {
     var testeMail = inputEmail.value
 
     if (testeMail.indexOf(".") > -1 && testeMail.indexOf("@") > -1) {
-        inputEmail.style.border = "2px solid green";
+        inputEmail.style.color = "#80b918";
         inputEmail.style.borderLeft = "none";
         inputEmail.style.borderTop = "none";
         inputEmail.style.borderRight = "none";
     }
     else if (testeMail == '') {
-        inputEmail.style.border = "2px solid #b8860b";
+        inputEmail.style.color = "#ffc300";
         inputEmail.style.borderLeft = "none";
         inputEmail.style.borderTop = "none";
-        inputEmail.style.borderRight = "none";
+        inputEmail.style.borderRight = "none";  
     }
 
     else {
-        inputEmail.style.border = "2px solid red";
+        inputEmail.style.color = "#d90429";
+        inputEmail.style.border = "2px solid #000633";
         inputEmail.style.borderLeft = "none";
         inputEmail.style.borderTop = "none";
         inputEmail.style.borderRight = "none";
@@ -50,23 +52,60 @@ function verificarCep() {
     var testeCep = inputCep.value
 
     if (testeCep.length == 8) {
-        inputCep.style.border = "2px solid green";
+        inputCep.style.color = "#80b918";
         inputCep.style.borderLeft = "none";
         inputCep.style.borderTop = "none";
         inputCep.style.borderRight = "none";
     }
     else if (testeCep == '') {
-        inputCep.style.border = "2px solid #b8860b";
+        inputCep.style.border = "2px solid #ffc300";
         inputCep.style.borderLeft = "none";
         inputCep.style.borderTop = "none";
         inputCep.style.borderRight = "none";
     }
 
     else {
-        inputCep.style.border = "2px solid red";
+        inputCep.style.color = "#d90429";
+        inputCep.style.border = "2px solid #000633";
         inputCep.style.borderLeft = "none";
         inputCep.style.borderTop = "none";
         inputCep.style.borderRight = "none";
+    }
+}
+
+function verificarCnpj() {
+    var cnpj = inputCnpj.value
+
+    if (cnpj.length == 18) {
+        inputCnpj.style.color = "#80b918";
+        inputCnpj.style.borderLeft = "none";
+        inputCnpj.style.borderTop = "none";
+        inputCnpj.style.borderRight = "none";
+    }
+    else if (cnpj == '') {
+        inputCnpj.style.border = "2px solid #ffc300";
+        inputCnpj.style.borderLeft = "none";
+        inputCnpj.style.borderTop = "none";
+        inputCnpj.style.borderRight = "none";
+    }
+
+    else {
+        inputCnpj.style.color = "#d90429";
+        inputCnpj.style.border = "2px solid #000633";
+        inputCnpj.style.borderLeft = "none";
+        inputCnpj.style.borderTop = "none";
+        inputCnpj.style.borderRight = "none";
+    }
+}
+
+function validarPorte(){
+    var porte = selectPorte;
+
+    
+    if(porte == 1 || porte == 2 || porte == 3){
+        selectPorte.style.color = "#80b918"
+    } else {
+        selectPorte.style.color = "#d90429"
     }
 }
 
