c12b36038e548ef5fba0eb675cb5ff814fd48787
Pedro <pedro.pires@sptech.school>
2022-11-02 17:31:11 -0300

    validações do cadastro de novo funcionário feitas

--- site/public/js/cadastroFuncionario.js
+++ site/public/js/cadastroFuncionario.js
@@ -64,13 +64,75 @@ function enviarCadFunc(){
 }
 
 function validarNome(){
-    var nome = document.getElementById('nomeCadastro');
+    var nome = nomeCadastro.value;
 
-    if(nome.value.length >= 3){
+    if(nome.length >= 3){
         spanNome.innerHTML = "Ok";
         spanNome.style.color = "#80b918";
     }else{
         spanNome.innerHTML = "Nome inválido";
+        spanNome.style.color = "#d90429";
+    }
+}
+
+function validarCpf(){
+    var cpf = cpfCadastro.value;
+
+    if(cpf.length == 14){
+        spanCpf.innerHTML = "Ok";
+        spanCpf.style.color = "#80b918";
+    }else{
+        spanCpf.innerHTML = "CPF inválido";
         spanCpf.style.color = "#d90429";
+    }
+}
+
+function validarEmail(){
+    var email = emailCadastro.value;
+
+    if (email.length > 5 && email.indexOf('@') > -1 && email.indexOf(".") > -1) {
+        spanEmail.innerHTML = "Ok";
+        spanEmail.style.color = "#80b918";
+    }
+
+    else {
+        spanEmail.innerHTML = "Email inválido";
+        spanEmail.style.color = "#d90429";
+    }
+}
+
+function validarSenha(){
+    var senha = senhaCadastro.value;
+
+    if(senha.length >= 8){
+        spanSenha.innerHTML = "Ok";
+        spanSenha.style.color = "#80b918"
+    }else{
+        spanSenha.innerHTML = "Senha inválida";
+        spanSenha.style.color = "#d90429"
+    }
+}
+
+function validarTelefone(){
+    var telefone = telefoneCadastro.value;
+
+    if(telefone.length == 15){
+        spanTelefone.innerHTML = "Ok";
+        spanTelefone.style.color = "#80b918"
+    }else{
+        spanTelefone.innerHTML = "Telefone inválido";
+        spanTelefone.style.color = "#d90429"
+    }
+}
+
+function validarCep(){
+    var cep = cepCadastro.value;
+
+    if(cep.length == 9){
+        spanCep.innerHTML = "Ok";
+        spanCep.style.color = "#80b918"
+    }else{
+        spanCep.innerHTML = "CEP inválido";
+        spanCep.style.color = "#d90429"
     }
 }
